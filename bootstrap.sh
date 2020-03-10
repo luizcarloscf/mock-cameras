15bce8cf3517dee0cce4575cc195443aa7830396
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2020-03-10 18:17:30 -0300

    add dockerignore and update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,10 @@ fi
 
 if [[ $EUID == 0 ]]; then
 
+  #removing OpenCV
   apt-get purge -y python-opencv python3-opencv
 
+  #installing packages for OpenCV
   packages=(python3 python3-dev \
     ca-certificates \
     libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
@@ -21,15 +23,21 @@ if [[ $EUID == 0 ]]; then
     apt-get update
     apt-get install --no-install-recommends -y ${packages[*]} 
 
+  #getting pip3
   if ! command -v pip3 > /dev/null; then 
     wget https://bootstrap.pypa.io/get-pip.py
     python3 get-pip.py
     rm -f get-pip.py
   fi
+  #installing openCV
+  apt-get install --no-install-recommends -y python3-opencv
 fi
 
 if [[ $EUID != 0 || -z ${wasnt_root} ]]; then
+  #remove opencv installed by pip3
   pip3 uninstall --yes opencv-python
+
+  #installing packages listed at requirements.txt
   pip3 install -r requirements.txt
-  apt-get install --no-install-recommends -y python3-opencv
+  
 fi
